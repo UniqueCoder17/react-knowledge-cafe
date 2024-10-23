cb54aa42487540e6a9105c321778c01fcbd7dc08
UniqueCoder17 <raihanalamr08@gmail.com>
2024-10-23 20:46:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Headre/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
